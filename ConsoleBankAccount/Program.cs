f70698abe5df680a7cb8415e874ac55bc772dc53
CHERIF Noura (Engie Digital) <JK6275@engie.com>
2023-03-29 15:28:01 +0200

    reorder solution

---
+++ ConsoleBankAccount/Program.cs
@@ -0,0 +1,20 @@
+﻿using BankAccountKata.Application;
+
+namespace ConsoleBankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new Clock();
+            var transactionRepository = new TransactionRepository(clock);
+            var consoleMock = new TestableConsole();
+            var printStatement = new StatementPrinter(consoleMock);
+            var bankAccount = new BankAccount(transactionRepository, printStatement);
+            bankAccount.deposit(1000);
+            bankAccount.deposit(2000);
+            bankAccount.withdraw(500);
+            bankAccount.printStatement();
+        }
+    }
+}
